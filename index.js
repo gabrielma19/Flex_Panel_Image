e4ed4afea8615316171315c68f21b5b8ecdd23fd
Gabriel Marcos <gabrielmareia@hotmail.com>
2017-03-29 18:58:38 -0300

    Refact project
    Project was refactored.

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-initialize(loadAfter);
+const selectors = '.panel';
 
-function loadAfter() {
-  function toggleOpen(){
-      panel.classList.toggle('open')
-      console.log('Funfa')
+expandableCards(selectors);
+
+
+/**
+ * Create expandable cards.
+ * @param {string} selectors
+ */
+function expandableCards(selectors) {
+  let cards = document.querySelectorAll(selectors);
+
+  Array
+    .from(cards)
+    .forEach((card, index, cards) => {
+      card.addEventListener('transitionend', toggleOpen);
+      card.addEventListener('click', () => {
+        const classy = 'open';
+
+        cards.forEach(card => card.classList.remove(classy));
+        card.classList.add(classy);
+      });
+    });
+
+  /**
+   * Toggle class based on flex transition property.
+   * @param {TransitionEvent} event
+   */
+  function toggleOpen(event){
+    /**
+     * Executed on element event scope.
+     * @this {HTMLElement}
+     **/
+
+    if (event.propertyName.includes('flex')) {
+      this.classList.toggle('open-active');
     }
-  
-  const selectors =  '.panel'
-  let elements = document.querySelectorAll(selectors);
-  Array.from(elements).forEach(panel => 
-      panel.addEventListener('click', toggleOpen));
-  
-  
-  
+  }
 }
-
